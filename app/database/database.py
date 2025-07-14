e2d50acfa075df23735d2b1a91db313b68d6cea1
PhilipeSousa <barrosphilipe07@gmail.com>
2025-07-14 16:27:23 -0300

    ss

--- app/database/database.py
+++ app/database/database.py
@@ -3,41 +3,45 @@ from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from string import Template
 import os
 
-DB_USER = os.getenv('USER')
-DB_PASSWORD = os.getenv('PASSWORD')
-DB_NAME = os.getenv('DB_NAME')
-DB_PORT = os.getenv('DB_PORT')
-HOST = os.getenv('ENV')
-#if not HOST == 'production':
-  #HOST = 'postgres'
+DB_USER     = os.getenv('PG_USER')       
+DB_PASSWORD = os.getenv('PG_PASSWORD')   
+DB_NAME     = os.getenv('DB_NAME')
+DB_PORT     = os.getenv('DB_PORT')
+HOST        = os.getenv('ENV')
 
-DB_URL = Template('postgresql+psycopg2://$user:$password@$host:$port/$db_name')
+DB_URL = Template(
+    'postgresql+psycopg2://$user:$password@$host:$port/$db_name'
+)
 
-connString = DB_URL.safe_substitute(user=DB_USER, password=DB_PASSWORD, host=HOST, port=DB_PORT, db_name=DB_NAME)
+connString = DB_URL.safe_substitute(
+    user=DB_USER,
+    password=DB_PASSWORD,
+    host=HOST,
+    port=DB_PORT,
+    db_name=DB_NAME
+)
+
 engine = create_engine(connString, echo=True)
 
 SessionLocal = sessionmaker(
-  autocommit=False,
-  autoflush=False,
-  bind=engine,
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
 )
 
-
 class Base(DeclarativeBase):
-  pass
-
+    pass
 
 def get_session():
-  db = SessionLocal()
-  try:
-    yield db
-  finally:
-    db.close()
-
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 def setup_db():
-  with engine.connect() as conn:
-    conn.execute(text('CREATE SCHEMA IF NOT EXISTS main'))
-    conn.commit()
+    with engine.connect() as conn:
+        conn.execute(text('CREATE SCHEMA IF NOT EXISTS main'))
+        conn.commit()
 
-  Base.metadata.create_all(engine)
+    Base.metadata.create_all(engine)
